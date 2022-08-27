11f2f4ddd162dd4b4e404063c9aa3d350f25d852
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-08-27 20:12:14 +0530

    Update __init__.py

--- program/__init__.py
+++ program/__init__.py
@@ -4,15 +4,11 @@ __version__ = "0.1"
 import time
 import logging
 
-from driver.core import me_bot
 
 logging.basicConfig(
-  filename=f'streambot-logs-{me_bot.id}.txt',
-  level=logging.INFO,
-  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
 )
-logging.getLogger("yt_dlp").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("PyTgCalls").setLevel(logging.ERROR)
 
 LOGS = logging.getLogger(__name__)
